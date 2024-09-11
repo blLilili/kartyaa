1309f67cf4b88ee715448d5bc4f795f67aaf0155
blLilili <biboklilivbj@gmail.com>
2024-09-11 13:48:52 +0200

    "notes"

--- js/roses.js
+++ js/roses.js
@@ -4,4 +4,20 @@ var, let -> let a = 23;
 let -> block scope
 var -> function scope
 állandó: const const a = 10;
+Elemi típusok:
+"sztring", 'string' `string (template) literal $változó`;
+number: 3, 3.12 parseInt(), number(), parseFloat();
+undefined (érték és egyben típus) -> nics definiálva, de majd lesz
+null -> vagy null a definiált dípusa vagy semmit adtak értékül az inputba, típusa ugyanúgy number
+operátorok: + (túlterhelt), -, *, /, %(modulo, modulus), = (értékadó)
+inkrementálás: ++ (python +=1)
+dekrementálás: -- (python -=1
+
+összehasonlító operátorok:
+<, >, <=, >=, ==, ===,
+
+logikai operátorok:
+negálás -> !, and -> &&, or -> ||
+
+A JS gyengén típusos nyelv: ==, 3 + "4" -> '34', 3*"4" -> 12
 */
